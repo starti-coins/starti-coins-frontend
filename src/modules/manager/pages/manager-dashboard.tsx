30ddd301628c8fcf927e3f90ea05f0157d3bbd11
JezzDiego <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>
2025-07-24 17:53:05 -0300

    feat: add task data to ManagerDashboard

--- src/modules/manager/pages/manager-dashboard.tsx
+++ src/modules/manager/pages/manager-dashboard.tsx
@@ -3,6 +3,22 @@ import Page from "@/components/@ui/page";
 import { SectionCards } from "../components/section-cards";
 import { Suspense } from "react";
 import { Spinner } from "@/components/@ui/spinner";
+import { TaskTable } from "@/modules/shared/task-table/data-table";
+import { Task } from "@/models/task";
+
+const data = {
+  id: 1,
+  title: "Tarefa 1",
+  description: "Descrição da tarefa 1",
+  status: "Done",
+  responsible_id: "Eddie Lake",
+  level: 3,
+  due_date: new Date(),
+  assignment_date: new Date(),
+  completion_date: new Date(),
+  hours: 2,
+  coins: 100,
+} satisfies Task;
 
 function ManagerDashboard() {
   return (
@@ -15,7 +31,8 @@ function ManagerDashboard() {
               <h3 className="font-medium text-xl">Atividade recente</h3>
 
               <Suspense fallback={<Spinner />}>
-                <RecentActivityList />
+                {/* <RecentActivityList /> */}
+                <TaskTable data={[data]} />
               </Suspense>
             </div>
           </div>
